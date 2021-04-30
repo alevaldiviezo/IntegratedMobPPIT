0bdd061ee0a7ccd9003d8c12044fd72f8003ca60
alevaldiviezo <73079613+alevaldiviezo@users.noreply.github.com>
2021-04-30 12:46:34 +0100

    Update App.js
    Function to start recordings added

--- App.js
+++ App.js
@@ -2,16 +2,31 @@ import React, { useState, useRef, useEffect } from 'react';
 import { View, StyleSheet, Button, Text } from 'react-native';
 import { Audio } from 'expo-av';
 
-let recording = new Audio.Recording();
-
 export default function App() {
   const [RecordedURI, SetRecordedURI] = useState('');
-  const [AudioPerm, SetAudioPerm] = useState(false);
-  const [isRecording, SetisRecording] = useState(false);
-  const [isPLaying, SetisPLaying] = useState(false);
-  const Player = useRef(new Audio.Sound());
 
-export default function App() {
+  const [recording, setRecording] = React.useState();
+  const [sound, setSound] = React.useState();
+
+  async function startRecording() {
+    try {
+      console.log('Requesting permissions..');
+      await Audio.requestPermissionsAsync();
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      }); 
+      console.log('Starting recording..');
+      const recording = new Audio.Recording();
+      await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
+      await recording.startAsync(); 
+      setRecording(recording);
+      console.log('Recording started');
+    } catch (err) {
+      console.error('Failed to start recording', err);
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
